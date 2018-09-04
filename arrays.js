ec27f80c29be8e16c5cf4ede713136d066444c60
ana.volaric95@gmail.com <ana.volaric95@gmail.com>
2018-09-04 12:42:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.slice(1)
+  newArray = array.slice(1)
+  return newArray
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
